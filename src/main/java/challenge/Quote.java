1ee7899596dcbd14631b26b9c683e7761cf165f2
Ricardo Lima <ricardoccofg@gmail.com>
2019-02-14 23:50:06 -0300

    funcionando

--- src/main/java/challenge/Quote.java
+++ src/main/java/challenge/Quote.java
@@ -1,10 +1,12 @@
 package challenge;
 
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "scripts")
@@ -12,20 +14,24 @@ public class Quote {
 
 	@Id
 	private Integer id;
-
 	private Integer episode;
 	private String episode_name;
 	private String segment;
 	private String type;
 	private String actor;
 	private String character;
-	private String detail;
+	@Column(name="detail")
+	private String quote;
 	private LocalDate record_date;
 	private String series;
 	private LocalDate transmission_date;
 
+	public Quote(){
+
+	}
+
 	public Quote(Integer id, Integer episode, String episode_name, String segment, String type, String actor,
-				 String character, String detail, LocalDate record_date, String series, LocalDate transmission_date) {
+			String character, String quote, LocalDate record_date, String series, LocalDate transmission_date) {
 		this.id = id;
 		this.episode = episode;
 		this.episode_name = episode_name;
@@ -33,101 +39,97 @@ public class Quote {
 		this.type = type;
 		this.actor = actor;
 		this.character = character;
-		this.detail = detail;
+		this.setQuote(quote);
 		this.record_date = record_date;
 		this.series = series;
 		this.transmission_date = transmission_date;
 	}
 
-	public Quote(){
-
+	public String getActor() {
+		return this.actor;
 	}
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
+	public String getCharacter() {
+		return this.character;
 	}
 
 	public Integer getEpisode() {
-		return episode;
-	}
-
-	public void setEpisode(Integer episode) {
-		this.episode = episode;
+		return this.episode;
 	}
 
 	public String getEpisode_name() {
-		return episode_name;
+		return this.episode_name;
 	}
 
-	public void setEpisode_name(String episode_name) {
-		this.episode_name = episode_name;
+	public Integer getId() {
+		return this.id;
+	}
+
+	public String getQuote() {
+		return this.quote;
+	}
+
+	public LocalDate getRecord_date() {
+		return this.record_date;
 	}
 
 	public String getSegment() {
-		return segment;
+		return this.segment;
 	}
 
-	public void setSegment(String segment) {
-		this.segment = segment;
+	public String getSeries() {
+		return this.series;
+	}
+
+	public LocalDate getTransmission_date() {
+		return this.transmission_date;
 	}
 
 	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getActor() {
-		return actor;
+		return this.type;
 	}
 
 	public void setActor(String actor) {
 		this.actor = actor;
 	}
 
-	public String getCharacter() {
-		return character;
-	}
-
 	public void setCharacter(String character) {
 		this.character = character;
 	}
 
-	public String getDetail() {
-		return detail;
+	public void setEpisode(Integer episode) {
+		this.episode = episode;
 	}
 
-	public void setDetail(String detail) {
-		this.detail = detail;
+	public void setEpisode_name(String episode_name) {
+		this.episode_name = episode_name;
 	}
 
-	public LocalDate getRecord_date() {
-		return record_date;
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setQuote(String quote) {
+		this.quote = quote;
 	}
 
 	public void setRecord_date(LocalDate record_date) {
 		this.record_date = record_date;
 	}
 
-	public String getSeries() {
-		return series;
+	public void setSegment(String segment) {
+		this.segment = segment;
 	}
 
 	public void setSeries(String series) {
 		this.series = series;
 	}
 
-	public LocalDate getTransmission_date() {
-		return transmission_date;
-	}
-
 	public void setTransmission_date(LocalDate transmission_date) {
 		this.transmission_date = transmission_date;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 }
